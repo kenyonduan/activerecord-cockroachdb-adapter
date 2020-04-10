053904626988834deb9f49d71c1466407aa6d141
duan <duan@easya.cc>
2020-04-10 14:44:59 +0800

    *  为 cockroachdb 重新注册所有的 type

--- lib/active_record/connection_adapters/cockroachdb_adapter.rb
+++ lib/active_record/connection_adapters/cockroachdb_adapter.rb
@@ -194,7 +194,25 @@ module ActiveRecord
         end
       end
 
+      ActiveRecord::Type.add_modifier({ array: true }, OID::Array, adapter: :cockroachdb)
+      ActiveRecord::Type.add_modifier({ range: true }, OID::Range, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:bit, OID::Bit, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:bit_varying, OID::BitVarying, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:binary, OID::Bytea, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:cidr, OID::Cidr, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:date, OID::Date, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:datetime, OID::DateTime, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:decimal, OID::Decimal, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:enum, OID::Enum, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:hstore, OID::Hstore, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:inet, OID::Inet, adapter: :cockroachdb)
       ActiveRecord::Type.register(:jsonb, OID::Jsonb, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:money, OID::Money, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:point, OID::Point, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:legacy_point, OID::LegacyPoint, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:uuid, OID::Uuid, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:vector, OID::Vector, adapter: :cockroachdb)
+      ActiveRecord::Type.register(:xml, OID::Xml, adapter: :cockroachdb)
 
       # end private
     end
